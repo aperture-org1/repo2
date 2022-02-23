8c78b3434e82d5705c735156d4def8b61bcc64bc
Gopi Dhulipalla <gdhulipalla@paloaltonetworks.com>
2022-02-22 16:40:04 -0800

    2nd commit - One sensittve and one sample file

---
+++ com/test/sensitive/SensitiveEmployee2.java
@@ -0,0 +1,98 @@
+public class Employee {  
+
+SSN - 234-34-2341
+
+private String name;  
+private String id;  
+private double sal;  
+public String getName() {  
+    return name;  
+}  
+public void setName(String name) {  
+    this.name = name;  
+}  
+public String getId() {  
+    return id;  
+}  
+public void setId(String id) {  
+    this.id = id;  
+}  
+public double getSal() {  
+    return sal;  
+}  
+public void setSal(double sal) {  
+    this.sal = sal;  
+}  
+public String getName() {  
+    return name;  
+}  
+public void setName(String name) {  
+    this.name = name;  
+}  
+public String getId() {  
+    return id;  
+}  
+public void setId(String id) {  
+    this.id = id;  
+}  
+public double getSal() {  
+    return sal;  
+}  
+public void setSal(double sal) {  
+    this.sal = sal;  
+}  
+public String getName() {  
+    return name;  
+}  
+public void setName(String name) {  
+    this.name = name;  
+}  
+public String getId() {  
+    return id;  
+}  
+public void setId(String id) {  
+    this.id = id;  
+}  
+public double getSal() {  
+    return sal;  
+}  
+public void setSal(double sal) {  
+    this.sal = sal;  
+}  
+public String getName() {  
+    return name;  
+}  
+public void setName(String name) {  
+    this.name = name;  
+}  
+public String getId() {  
+    return id;  
+}  
+public void setId(String id) {  
+    this.id = id;  
+}  
+public double getSal() {  
+    return sal;  
+}  
+public void setSal(double sal) {  
+    this.sal = sal;  
+}  
+public String getName() {  
+    return name;  
+}  
+public void setName(String name) {  
+    this.name = name;  
+}  
+public String getId() {  
+    return id;  
+}  
+public void setId(String id) {  
+    this.id = id;  
+}  
+public double getSal() {  
+    return sal;  
+}  
+public void setSal(double sal) {  
+    this.sal = sal;  
+}  
+}  
