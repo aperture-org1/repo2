379cd61091f3cf62afa20dd4cc6972a9d535d777
Aperture QA <apertureqa@users.noreply.github.com>
2022-04-08 11:11:31 -0700

    Updated ssns

--- File1.java
+++ File1.java
@@ -4,7 +4,7 @@ public sttatic void  main()  {
 }
 
 private static printSensitive() {
-    SSN - 123-23-2343
-    SSN - 123-23-2344
+    SSN - 123-23-2341
+    SSN - 123-23-2342
     AMEX - 123243329832124 123243329832124 123243329832124
 }
