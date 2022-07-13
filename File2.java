ac4c566f53de53a2de18535ed440d2be4b6b55a7
Aperture QA <apertureqa@users.noreply.github.com>
2022-07-13 15:28:19 -0700

    Stored SSN

--- File2.java
+++ File2.java
@@ -3,6 +3,5 @@ Sample java File
   SSN-1 - 123-23-2350
   SSN-2 - 123-23-2352
   SSN-3 - 123-23-2352
-
-    SSN-4 - 123-23-2461 - New SSN  
-    SSN-5 - 123-23-2462 - Updated SSN
+  SSN-4 - 123-23-2461
+  SSN-5 - 123-23-2462
